b486705bb693a885f50f7dc64b5581bcf8da38f9
dhatrigarg <dhatrigarg67@gmail.com>
2026-07-08 18:23:08 +0530

    Completed rock paper scissors game

--- RockPaperScissors/rpsScript.js
+++ RockPaperScissors/rpsScript.js
@@ -1,46 +1,47 @@
-let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
+
+let Rock = document.getElementById("rock");
+let Paper = document.getElementById("paper");
+let Scissors = document.getElementById("scissors");
+let resultBtn = document.querySelector(".btn");
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+let winScore = document.getElementById("winRate")
 let userScore = 0;
 let botScore = 0;
+let winPercent = 0;
 
 rock.addEventListener("click", function() {
-    playGame("rock");
+    playGame("Rock");
 });
-
 paper.addEventListener("click", function() {
-    playGame("paper");
+    playGame("Paper");
 });
-
 scissors.addEventListener("click", function() {
-    playGame("scissors");
+    playGame("Scissors");
 });
-
+        
 function playGame(userChoice) {
-
-    let choices = ["rock", "paper", "scissors"];
+    let choice_list=["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * 3);
-    let computerChoice = choices[randomIndex];
+    let computerChoice = choice_list[randomIndex];
 
     if (userChoice === computerChoice) {
-        resultBtn.innerText = "It's a Draw!";
-    }
-    else if (
-        (userChoice === "rock" && computerChoice === "scissors") ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
-    ) {
+        resultBtn.innerText = "Its a Draw";
+    } else if (
+        (userChoice==="Rock" && computerChoice==="Scissors")  || 
+        (userChoice==="Paper" && computerChoice==="Rock") || 
+        (userChoice==="Scissors" && computerChoice==="Paper")){
+        resultBtn.innerText = `You win! ${userChoice} beats ${computerChoice}. `;
         userScore++;
-        userScorePara.innerText = userScore;
-        resultBtn.innerText = `You Win! ${userChoice} beats ${computerChoice}`;
-    }
-    else {
-        botScore++;
-        botScorePara.innerText = botScore;
+        playerScore.innerText = userScore;
+    } else {
         resultBtn.innerText = `You Lose! ${computerChoice} beats ${userChoice}`;
+        botScore++;
+        computerScore.innerText = botScore;
+    }
+
+    if (userScore!=0) {
+        winPercent=((userScore*100)/(userScore+botScore)).toFixed(2);
+        winScore.innerText=winPercent;
     }
 }
-let resultBtn = document.querySelector(".result button");
-
-let userScorePara = document.getElementById("userScore");
-let botScorePara = document.getElementById("botScore");
